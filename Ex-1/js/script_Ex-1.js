a6ec819a1126bc2453bccf8a80fd2ce1f77cec53
Karllos Eduardo <karlloseduardopereira@gmsil.com>
2022-11-18 18:25:40 -0300

    first commit

---
+++ Ex-1/js/script_Ex-1.js
@@ -0,0 +1,7 @@
+
+const salario = prompt('Digite seu sálario:');
+const aumento = prompt('Digite a porcentagem do aumento:');
+const resulatdo = Number(salario) * (1 + Number(aumento)/100);
+Math.round(2);
+document.write(resulatdo);
+alert(resulatdo);
